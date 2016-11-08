046c5fe7de0341227681dc3081a45c6765658d30
tempestium <dennisvanbenschop@hotmail.com>
2016-11-08 15:21:38 +0100

    test push/commit

--- Miniproject/test.py
+++ Miniproject/test.py
@@ -1,1 +1,16 @@
-print("hellow git")
+from tkinter import *
+
+root = Tk('test')
+
+label = Label(master= root, text= 'hellowworld', background='blue')
+label.pack()
+
+button = Button(master= root, text='druk op mij, me likes')
+button.pack(pady=20)
+
+entry = Entry(master= root)
+
+
+root.mainloop()
+
+
