8fba3358906ea273e174c0ccc6097f9781d97d7a
tempestium <dennisvanbenschop@hotmail.com>
2016-11-10 00:56:01 +0100

    inlog + nieuw aanmelden werkend.

--- Super-Wonder-Captain/frametest.py
+++ Super-Wonder-Captain/frametest.py
@@ -1,10 +1,11 @@
 from tkinter import *
+from tkinter.messagebox import showinfo
 import csv
 
 def raise_frame(frame):
     frame.tkraise()
 
-
+# de functie voor het inloggen
 def inloggen():
 
     list = []
@@ -41,16 +42,41 @@ def inloggen():
     else:
         print("het wachtwoord of de gebruiksnaam is fout")
 
+# de functie voor het aanmaken van een nieuwe gebruiker/speler
+def nieuwUser():
+    list = []
+
+    with open("login.csv", "r") as myCSVFile:
+        reader = csv.reader(myCSVFile, delimiter=";")
+        for row in reader:
+            for field in row:
+                list.append(field)
+        myCSVFile.close()
+
+        if nWachtwrd.get() == cWachtwrd.get():
+            while True:
+                naam = nGebruiksnaam.get()
+
+                if naam in list:
+                    showinfo(title='attentie!', message='uw gekozen naam is al gekozen, kiest u alstublieft een andere.')
+
+                else:
+                    wachtwoord = nWachtwrd.get()
+                with open("login.csv", "a") as meCSVFile:
+                    writer = csv.writer(meCSVFile, delimiter=";")
+                    writer.writerow((naam, wachtwoord))
+                    raise_frame(inlog)
+                    break
 
 
+# de knop om de hoghscore lijst van "score" naar "Tijd" te veranderen
 def highscoretoggle():
     global test_text
     test_text ='testtest'
     raise_frame(highscores)
     return
 
-
-
+# main code en opbouw van de GUI
 root = Tk()
 
 inlog = Frame(root)
@@ -64,7 +90,7 @@ for frame in (inlog,aanmelden, mainMenu, game, highscores):
     frame.grid(row=0, column=0, sticky='news')
 
 # frame inlog
-Label(inlog, text='super-woman-Captain', font='bold 20').pack(pady=5)
+Label(inlog, text='Super-Wonder-Captain', font='bold 20').pack(pady=5)
 logo = PhotoImage(file=r'superman.gif')
 Label(inlog, image=logo,).pack()
 Label(inlog, text='click op de knop om de game te starten').pack(pady=10)
@@ -83,19 +109,21 @@ logo1 = PhotoImage(file=r'superman.gif')
 Label(aanmelden, image=logo1,).pack()
 Label(aanmelden, text= "voer gebruikers naam en wachtwoordt in.").pack()
 Label(aanmelden, text= 'gebruikersnaam').pack()
-Entry(aanmelden).pack()
+nGebruiksnaam = Entry(aanmelden)
+nGebruiksnaam.pack()
 Label(aanmelden, text= 'wachtwoord').pack()
-Entry(aanmelden).pack()
+nWachtwrd = Entry(aanmelden)
+nWachtwrd.pack()
 Label(aanmelden, text= 'bevestig wachtwoord').pack()
-Entry(aanmelden).pack()
+cWachtwrd = Entry(aanmelden)
+cWachtwrd.pack()
 
-Button(aanmelden, text= "aanmelden", command=lambda:raise_frame(inlog)).pack(pady=10,side='left')
-Button(aanmelden, text= "terug", command=lambda:raise_frame(inlog)).pack(pady=10,side='right')
+Button(aanmelden, text= "aanmelden", command=lambda:nieuwUser()).pack(pady=10)
+Button(aanmelden, text= "terug", command=lambda:raise_frame(inlog)).pack(pady=10)
 
 #frame main menu
 logo2= PhotoImage(file=r'batman.gif')
 Label(mainMenu, image=logo2).pack()
-
 Label(mainMenu, text='the game, you lost').pack()
 Button(mainMenu, text='start game', command=lambda:raise_frame(game)).pack()
 Button(mainMenu, text='highscore', command=lambda:raise_frame(highscores)).pack()
